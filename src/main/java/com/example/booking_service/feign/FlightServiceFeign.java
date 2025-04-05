3e61f67a4fccb4b3e215aab8a36775c0af8f72a0
amanglani2503 <ambermanglani8871@gmail.com>
2025-04-05 18:24:46 +0530

    [Amber]. Added Feign client to communicate with FlightService

--- src/main/java/com/example/booking_service/feign/FlightServiceFeign.java
+++ src/main/java/com/example/booking_service/feign/FlightServiceFeign.java
@@ -1,10 +1,9 @@
 package com.example.booking_service.feign;
 
+import com.example.booking_service.entity.Booking;
+import com.example.booking_service.entity.FlightDetails;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 @FeignClient(name = "FLIGHT-SERVICE")
 public interface FlightServiceFeign {
@@ -13,8 +12,14 @@ public interface FlightServiceFeign {
     boolean isSeatAvailable(@RequestParam Long flightId,
                             @RequestHeader("Authorization") String token);
 
-    @PutMapping("/flights/update-seats")
-    String updateSeatAvailability(@RequestParam Long flightId,
-                                @RequestParam boolean isBooking,
-                                @RequestHeader("Authorization") String token);
+    @PutMapping("/flights/book-seats")
+    FlightDetails bookSeat(@RequestParam Long flightId,
+                              @RequestHeader("Authorization") String token);
+
+    @PutMapping("/flights/cancel-seat")
+    void cancelSeat(@RequestParam Long flightId,
+                    @RequestParam String seatNumber, @RequestHeader("Authorization") String authHeader);
+
+    @GetMapping("flights/getDetails")
+    FlightDetails getFlightDetails(@RequestParam Long flightId, @RequestHeader("Authorization") String authHeader);
 }
