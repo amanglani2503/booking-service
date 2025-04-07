69c58a1376d5e95ad5f1d62f708b4a9345d716aa
amanglani2503 <ambermanglani8871@gmail.com>
2025-04-08 00:07:33 +0530

    [Amber]. Added PaymentServiceFeign

---
+++ src/main/java/com/example/booking_service/feign/PaymentServiceFeign.java
@@ -0,0 +1,16 @@
+package com.example.booking_service.feign;
+
+import com.example.booking_service.entity.StripeResponse;
+import com.example.booking_service.entity.PaymentRequest;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+
+@FeignClient(name = "PAYMENT-SERVICE")
+public interface PaymentServiceFeign {
+
+    @PostMapping("/pay/checkout")
+    StripeResponse makePayment(@RequestBody PaymentRequest request,
+                               @RequestHeader("Authorization") String token);
+}
